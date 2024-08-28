0b086bac71e746a8a798b01a953366ad1d71970a
vadke <vadker6@mail.ru>
2024-08-28 20:31:13 +0500

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,58 @@
+import java.util.Scanner;
 
+class Car
+{
+    String name;
+    int speed;
+
+    public Car(String name, int speed)
+    {
+        this.name = name;
+        this.speed = speed;
+    }
+}
+
+class Race
+{
+    String leader = "";
+    int distance1 = 0;
+    public void Leader(Car car)
+    {
+        int distance2 = car.speed * 24;
+        if(distance2 > distance1)
+        {
+            distance1 = distance2;
+            leader = car.name;
+        }
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+
+        for(int i = 0; i < 3; i++)
+        {
+            System.out.println("Введите название автомобиля");
+            String name = scanner.next();
+            int speed;
+            while(true)
+            {
+                System.out.println("Введите скорость автомобиля от 0 до 250");
+                speed = scanner.nextInt();
+
+                if(speed > 0 && speed < 250)
+                {
+                    break;
+                }
+                else
+                {
+                    System.out.println("Ошибка! Попробуйте еще раз");
+                }
+            }
+            Car car = new Car(name, speed);
+            race.Leader(car);
+        }
+        System.out.println("Самая быстрая машина:" + race.leader);
     }
 }
